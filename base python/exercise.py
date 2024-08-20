661ee4720c00c667e27d99be9a81984291c08977
zaeem-shakeel <zaeemshakeel6428@gmail.com>
2024-08-20 11:16:08 -0700

    add

--- base python/exercise.py
+++ base python/exercise.py
@@ -1,25 +1,21 @@
-
-st=input("enter message")
-words=st.split(" ")
-codind=True
-if(coding):
-    nwords=[]
+st = input("enter  message :")
+words = st.split(" ")
+coding = False
+if coding:
+    nwords =[]
     for word in words:
-        if(len(word)>=3):
-            r1="dsf"
-            r2="jkr"
-            stnew=r1+ word[1:]+word[0]+r2
-            nword.append(strnew)
+        if len(word) >= 3:
+            r1 = "dsf"
+            r2 = "jkr"
+            stnew = r1 + word[1:] + word[0] + r2
+            nwords.append(stnew)
         else:
-            nword.append(word[::-1])
-            print(" ".join(nword))
+            nwords.append(word[::-1])
+    print(" ".join(nwords))
 else:
-    nwords=[]
+    nwords = []
     for word in words:
-        stnew=word[3:-3]
-        stnew=stnew[-1]+stnew[:-1]
+        stnew = word[3:-3]
+        stnew = stnew[-1] + stnew[:-1]
         nwords.append(stnew)
-    else:
-        nwords.append(word[::-1])
-        print(" ".join(nwords))
-            
+    print(" ".join(nwords))
